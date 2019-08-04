6f5a7d74e65f8b1470b67778eb07cb037e0aad85
herolewis <lewiskycc@hotmail.com>
2019-08-04 21:14:31 +0800

    Updates

---
+++ precache-manifest.236c12e7a7f6d62ba8748b390482dcdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e359bd6dd382365441e9e179c785f277",
+    "url": "/lan-web/index.html"
+  },
+  {
+    "revision": "96e2ad49ed032405fdb8",
+    "url": "/lan-web/static/css/2.862259ab.chunk.css"
+  },
+  {
+    "revision": "e4bd203cca24eb21a3ea",
+    "url": "/lan-web/static/css/main.936a89be.chunk.css"
+  },
+  {
+    "revision": "96e2ad49ed032405fdb8",
+    "url": "/lan-web/static/js/2.e97ca0b7.chunk.js"
+  },
+  {
+    "revision": "79d3149fbe65070eb6a5",
+    "url": "/lan-web/static/js/3.139b426c.chunk.js"
+  },
+  {
+    "revision": "abc3294ed9ba86d5cc10",
+    "url": "/lan-web/static/js/4.33a0fee4.chunk.js"
+  },
+  {
+    "revision": "e4bd203cca24eb21a3ea",
+    "url": "/lan-web/static/js/main.712bb04b.chunk.js"
+  },
+  {
+    "revision": "dc91920541f2efc70690",
+    "url": "/lan-web/static/js/runtime~main.5b8687c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lan-web/precache-manifest.bcd10ce6e0d8baafc299f58cc67d83a0.js"
+  "/lan-web/precache-manifest.236c12e7a7f6d62ba8748b390482dcdc.js"
 );
 
 self.addEventListener('message', (event) => {
